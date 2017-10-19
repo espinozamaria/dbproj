e7c1f773c892b926693e56dddcd57fbbefa50795
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-10-18 17:59:39 -0700

    Working on the searching portion still...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import{ FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Pipes
+import{ SearchByPipe } from './pipes/search-by.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchByPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
